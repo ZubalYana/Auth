2e6fbefef87ead7a26e01928f0c486fe838aca85
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-28 17:48:44 +0300

    create jwt secret

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
+const JWT_SECRET = 'secret_jwt_words'
 const app = express()
 app.listen(PORT, ()=>{
     console.log(`Server works on PORT: ${PORT}`)
