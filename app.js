26ade09056a0cd17dfa3c9ab9c64b2a27da54e87
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-28 18:10:03 +0300

    create userSchema

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ mongoose.connect(`mongodb+srv://root:CdLeejqYl0BDpKLE@cluster0.raizszz.mongodb.n
     console.log(`Connected to mongoDB`)
 })
 
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(express.static('public'));
+  
+
 app.listen(PORT, ()=>{
     console.log(`Server works on PORT: ${PORT}`)
 })
