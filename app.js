def066dd28b9192c99908959138ddf2e56ff1765
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-28 17:43:33 +0300

    run server and install all the required packages

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('exress')
+const path = require('path')
+const PORT = 3000;
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const app = express()
+app.listen(PORT, ()=>{
+    console.log(`Server works on PORT: ${PORT}`)
+})
