b979a7beb19abb6c0c4927b57583820c896ff763
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-06-28 18:40:28 +0300

    create authMiddleware

--- app.js
+++ app.js
@@ -62,7 +62,20 @@ const userSchema = new mongoose.Schema({
 
   });
   
-
+  const authMiddleware = (req, res, next) => {
+    const token = req.cookie.token;
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.userId = decoded.userId;
+      next();
+    } catch (err) {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  };
+  
 app.listen(PORT, ()=>{
     console.log(`Server works on PORT: ${PORT}`)
 })
